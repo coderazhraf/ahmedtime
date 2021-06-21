2db875670bd9d0534870326c1e70ac953325a427
coderazhraf <azhrafahmed8@gmail.com>
2021-06-21 09:51:00 +0200

    second commit

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 let open = document.querySelector(".open");
 let close = document.querySelector(".close");
 let navLink = document.querySelector(".navLink");
+let logo = document.querySelector(".logo");
 
 open.addEventListener("click", ()=>{
     console.log(navLink)
     navLink.style.left = 0;
     open.style.display = "none";
     close.style.opacity = 1;
-
+    logo.classList.add("logodesing");
 });
 close.addEventListener("click", ()=>{
     console.log(navLink)
     navLink.style.left = -100 + "%";
     open.style.display = "block";
     close.style.opacity = 0;
+    logo.classList.remove("logodesing");
 
 });
